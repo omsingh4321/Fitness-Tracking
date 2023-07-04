1b442ec02c02482dd4a53e3235b88e742d779682
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-07-05 00:05:31 +0530

    Update SignUp.js

--- BackEnd/modals/SignUp.js
+++ BackEnd/modals/SignUp.js
@@ -1,1 +1,14 @@
+const mongoose=require('mongoose');
+const { Schema } = mongoose;
 
+const SignUpSchema = new Schema({
+  name: String, // String is shorthand for {type: String}
+  dob: String,
+  email: String,
+  password: String
+
+});
+
+const SignUp = mongoose.model('SignUp', SignUpSchema);
+
+module.exports=SignUp
