27a04bb87c2e44849609e5f3d1eb6abff0c07e42
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-07-04 23:51:29 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,1 +1,25 @@
 
+const dotenv=require("dotenv")
+ dotenv.config();
+const conn = require('./connections/mongoConn.js');
+conn();
+
+const express=require('express');
+const cors= require('cors');
+const app= express();
+const authRouter=require('./modules/auth/router.js');
+app.use(express.json()); //To get the data in json format in  frontend for postman
+app.use(express.urlencoded({extended:true}));// taking data from frontEnd
+
+app.use(cors())
+
+app.use('/omApi',authRouter);
+
+app.listen(3000,()=>{
+    console.log("app is listening on 3000")
+})
+
+
+
+
+  
