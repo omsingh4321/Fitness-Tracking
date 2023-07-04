9e95c840f57c427797bca15f03f5cd5101473294
Om Singh <110286904+omsingh4321@users.noreply.github.com>
2023-07-05 00:04:18 +0530

    Update mongoConn.js

--- BackEnd/connections/mongoConn.js
+++ BackEnd/connections/mongoConn.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const fun=async()=>{
+    try {
+        const conn=await  mongoose.connect(process.env.ConnectionMongoUrl);
+        console.log("mongoose connected");
+    } catch (error) {
+        console.log("ERROR::",error.message)
+    }
+}
+
+module.exports=fun;
 
